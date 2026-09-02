1c306a7f00db7f99063f353e6296150493b73c49
Maxim Loukinov <mloukino@terpmail.umd.edu>
2026-09-02 11:35:14 -0400

    Initial commit of full file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Maxim Loukinov!'
+    return 'Hello World from Maxim Loukinov!!'
 
 
 if __name__ == '__main__':
